ff5327fc558b8ab8006b6b29fe601951abd57853
lion2511996666 <50170217+lion2511996666@users.noreply.github.com>
2019-05-01 23:38:34 +0200

    Add files via upload

---
+++ cashier/includes/header.php
@@ -0,0 +1,38 @@
+<div class="header">
+	<div class="headerleft">
+		<b>Hospital Management System</b>
+	</div>
+	<div class="headerright">
+		<b>
+			<?php
+			require '../includes/connect.php';
+			require '../includes/users.php';
+			cashierdetails();
+			 ?>
+		</b>
+	</div>
+</div>
+
+<html>
+<head><title>Put Image in HTML</title>
+
+</head>
+
+<center>
+<body background = "">
+
+<img src = "includes/cashier-header2.jpg" width="95%" />
+
+</body></center>
+</html>
+<html>
+	<style type="text/css">
+	body
+	{
+
+		background-color: #1E90FF;
+	}
+		
+	</style>
+</head>
+</html>
---
+++ cashier/includes/header1.php
@@ -0,0 +1,38 @@
+<div class="header">
+	<div class="headerleft">
+		<b>Hospital Management System</b>
+	</div>
+	<div class="headerright">
+		<b>
+			<?php
+			require '../includes/connect.php';
+			require '../includes/users.php';
+			cashierdetails();
+			 ?>
+		</b>
+	</div>
+</div>
+
+<html>
+<head><title>Put Image in HTML</title>
+
+</head>
+
+<center>
+<body background = "">
+
+<img src = "includes/settings.jpg" width="1100px" height="330px"/>
+
+</body></center>
+</html>
+<html>
+	<style type="text/css">
+	body
+	{
+
+		background-color: #1E90FF;
+	}
+		
+	</style>
+</head>
+</html>
